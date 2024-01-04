8e9b98e6cd0493d823116409f9a1d7d6f5883e4b
Kevin Pedro <kpedro88@gmail.com>
2024-01-04 15:51:52 -0600

    make get_inspire more robust

--- get_inspire.sh
+++ get_inspire.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-grep '\item' src/pub.tex | grep '%' | cut -d'%' -f2 | tr '\r\n' ' ' | sed 's/   / OR /g'
+grep '\item' src/pub.tex | grep '%' | cut -d'%' -f2 | sed 's/ //; s/^/recid:/' | tr '\r\n' ' ' | sed 's/  recid:/ OR recid:/g'
