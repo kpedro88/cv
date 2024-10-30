b15a5ccba37581cfc091596beaf83cd65f804b83
Kevin Pedro <kpedro88@gmail.com>
2024-10-30 23:09:53 +0100

    automate redirect to citations on inspire

--- get_inspire.sh
+++ get_inspire.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-grep '\item' src/pub.tex | grep '%' | cut -d'%' -f2 | sed 's/ //; s/^/recid:/' | tr '\r\n' ' ' | sed 's/  recid:/ OR recid:/g'
+grep '\\item' src/pub.tex | grep '%' | cut -d'%' -f2 | sed 's/ //; s/^/recid:/' | tr '\r\n' ' ' | sed 's/  recid:/%20OR%20recid%3A/g' | sed -e 's~^~RedirectMatch 302 ^/kjp/cv/inspire$ https://inspirehep.net/literature?sort=mostrecent\&size=25\&page=1\&ui-citation-summary=true\&q=~' > .htaccess
